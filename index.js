66f7db4efd7c5caca2207c6c0722bf88e12bc9ac
Adam Duff <j.adam.duff@gmail.com>
2018-10-26 03:15:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ class Driver {
   }
 
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
