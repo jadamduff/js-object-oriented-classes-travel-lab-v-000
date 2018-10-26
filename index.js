3e637db8376fad34ffea5cfbf66e83fc0c3d65f0
Adam Duff <j.adam.duff@gmail.com>
2018-10-26 03:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Driver {
+  constructor(name, dateStarted) {
+    this.name = name;
+    this.startDate = new Date(dateStarted);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+
+}
